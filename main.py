668695abbde7522b3da508ac8630ca2df195e861
unknown <20alexchernik@gmail.com>
2021-08-05 19:58:55 +0300

    compliting parse_single_project, checking parse_single_page_with_projects

--- main.py
+++ main.py
@@ -4,33 +4,54 @@ import lxml
 from bs4 import BeautifulSoup
 # https://www.fl.ru/projects/?page=2&kind=1
 
-fr_ru_projects_url = "https://www.fl.ru/projects/"
+fl_ru_host = "https://www.fl.ru"
+fl_ru_projects_url = "https://www.fl.ru/projects/"
 
 
-async def parse_single_project(project_url, categories1=[], categories2=[]):
+async def parse_single_project(project_url, required_categories=[]):
+    """
+    Take data from a single page with a project description.
+    !!!In case of updating site you need to review soup.find_all!!!
+    :param project_url:
+    :param required_categories:
+    :return:
+    """
     async with aiohttp.ClientSession() as session:
         async with session.get(project_url) as resp:
             html = await resp.text()
             soup = BeautifulSoup(html, 'lxml')
 
-            categories1_2 = soup.find("div", class_=["b-layout__txt",
-                                                 "b-layout__txt_fontsize_11",
-                                                 "b-layout__txt_padbot_20"]).find_all("a")
-
-            category1 = categories1_2[0].text  # may not work
-            category2 = categories1_2[1].text  # may not work
+            project_categories_soup = soup.find_all("div", class_=["b-layout__txt",
+                                                                   "b-layout__txt_fontsize_11",
+                                                                   "b-layout__txt_padbot_20"])[9]
+            project_categories = [i.text for i in project_categories_soup.find_all("a")]
 
             title = soup.find("h1", class_="b-page__title").text
-            description = soup.find("div", class_="b-layout__txt b-layout__txt_padbot_20").text
-            price = soup.find("span", class_="b-layout__bold").text  # may not work
-            if category1 in categories1 and category2 in categories2:
-                pass
-            else:
-                pass
+            description = soup.find_all("div", class_=["b-layout__txt", "b-layout__txt_padbot_20"])[7].text.strip()
+            price = soup.find("span", class_="b-layout__bold").text.strip()  # may not work
+            project_id = project_url.split('/')[4]
+            print(project_id)
+            print(title)
+            print(description)
+            print(price)
+
+
+async def parse_single_page_with_projects(page_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(page_url) as resp:
+            html = await resp.text()
+            soup = BeautifulSoup(html, 'lxml')
+
+            project_titles_with_links = soup.find_all("h2", class_=["b-post__title",
+                                                                    "b-post__grid_title p-0",
+                                                                    "b-post__pin"])
+            project_links = [i.find("a").get("href") for i in project_titles_with_links]
+            for i in project_links:
+                print(i)
 
 
 async def main():
-    await parse_single_project("https://www.fl.ru/projects/4809533/parser-internet-magazina.html")
+    await parse_single_page_with_projects("https://www.fl.ru/projects/?kind=1")
 
 
 if __name__ == '__main__':
